0511f83c7be34653afbcd061f8f7c0f842be0dd7
harishmunjulur <harishmunjulur@gmail.com>
2020-02-07 14:50:31 -0800

    eck if nose is present
    Signed-off-by: harishmunjulur <harishmunjulur@gmail.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -5,13 +5,13 @@ relpath=$(dirname $0)/../../../src/test/pybind
 source $(dirname $0)/../ceph-helpers-root.sh
 install python2-nose
 
-find / -type f -name 'nosetests' -print -quit
+ls /usr/bin
 
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --errors-for-leak-kinds=definite --error-exitcode=1 \
     nosetests -v $relpath/test_rbd.py
 else
-  nose3 -v $relpath/test_rbd.py
+  nosetests -v $relpath/test_rbd.py
 fi
 exit 0
