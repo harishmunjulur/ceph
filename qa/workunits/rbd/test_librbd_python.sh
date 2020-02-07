116ed9591a3c495f8f2ccfd6e7dcfa8b60538ab9
harishmunjulur <harishmunjulur@gmail.com>
2020-02-07 11:12:52 -0800

    ecking path of nosetests
    Signed-off-by: harishmunjulur <harishmunjulur@gmail.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -5,6 +5,8 @@ relpath=$(dirname $0)/../../../src/test/pybind
 source $(dirname $0)/../ceph-helpers-root.sh
 install python2-nose
 
+find / -type f -name 'nosetests' -print -quit
+
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --errors-for-leak-kinds=definite --error-exitcode=1 \
