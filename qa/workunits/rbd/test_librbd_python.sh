e92a4a62d412e6cec69b44c07382b7fe26408be6
harishmunjulur <harishmunjulur@gmail.com>
2020-02-10 11:08:25 -0800

    removed unwnted code
    Signed-off-by: harishmunjulur <harishmunjulur@gmail.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -2,11 +2,6 @@
 
 relpath=$(dirname $0)/../../../src/test/pybind
 
-source $(dirname $0)/../ceph-helpers-root.sh
-install python2-nose
-
-ls /usr/bin
-
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --errors-for-leak-kinds=definite --error-exitcode=1 \
