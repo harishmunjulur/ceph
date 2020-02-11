fa88939929e78185fd159873086e9872e959a0d0
harishmunjulur <harishmunjulur@gmail.com>
2020-02-11 14:40:01 -0800

    version check
    Signed-off-by: harishmunjulur <harishmunjulur@gmail.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -2,11 +2,19 @@
 
 relpath=$(dirname $0)/../../../src/test/pybind
 
+pycmd=$"nosetests"
+
+if [ "$VERSION_ID" == "8.0" ]; then
+        pycmd=$"python3 -m nose"
+fi
+echo "harish"
+echo "$pycmd"
+
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --errors-for-leak-kinds=definite --error-exitcode=1 \
-   python3 -m nose -v $relpath/test_rbd.py
+   $pycmd -v $relpath/test_rbd.py
 else
-  python3 -m nose -v $relpath/test_rbd.py
+  $pycmd -v $relpath/test_rbd.py
 fi
 exit 0
