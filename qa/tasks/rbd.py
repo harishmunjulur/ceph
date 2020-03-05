52b6e16eebc04148ffdae0a89be9306f1c44cd2f
harishmunjulur <harishmunjulur@gmail.com>
2020-03-04 16:31:20 -0800

    anges with comens `
    Signed-off-by: harishmunjulur <harishmunjulur@gmail.com>

--- qa/tasks/rbd.py
+++ qa/tasks/rbd.py
@@ -71,8 +71,8 @@ def create_image(ctx, config):
                 '--size', str(size),
                 name,
                 '--image-format', '2', 
-                '--image-feature', 'layering'
-                '--image-feature', 'exclusive-lock'
+                '--image-feature', 'layering',
+                '--image-feature', 'exclusive-lock',
             ]
         # omit format option if using the default (format 1)
         # since old versions of don't support it
