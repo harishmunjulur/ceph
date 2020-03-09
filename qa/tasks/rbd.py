b4ba3f267f9d41a0c4dd8b36de0a06e521b4bc86
harishmunjulur <harishmunjulur@gmail.com>
2020-03-09 10:30:58 -0700

    changing te input parameters
    Signed-off-by: harishmunjulur <harishmunjulur@gmail.com>

--- qa/tasks/rbd.py
+++ qa/tasks/rbd.py
@@ -72,7 +72,6 @@ def create_image(ctx, config):
                 name,
                 '--image-format', '2', 
                 '--image-feature', 'layering',
-                '--image-feature', 'exclusive-lock',
             ]
         # omit format option if using the default (format 1)
         # since old versions of don't support it
